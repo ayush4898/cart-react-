4d23c2388f579dd948af7266a2594c17b8f033f5
ayush goel <ayush4898@gmail.com>
2021-04-08 22:57:54 +0530

    added cart list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Cartitem from './CartItem';
+import Cart from './Cart';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World!!</h1>
-      <Cartitem />
+      <Cart />
     </div>
   );
 }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,21 +1,59 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img : ''
+        }
+    }
+    increaseQty = () => {
+        // console.log('this', this.state.qty);
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1
+            }
+        });
+    }
+    decreaseQty = () => {
+        // console.log('this', this.state.qty);
+        if (this.state.qty > 0) {
+            this.setState((prevState) => {
+                return {
+                    qty: prevState.qty - 1
+                }
+            });
+        }
+    }
     render() {
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img alt="" style={style.image} />
+                    <img alt="" style={style.image}  src={this.state.img}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: '#777'}}>Price: 999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{ fontSize: 25 }}>{this.state.title }</div>
+                    <div style={{ color: '#777' }}>{ this.state.price}</div>
+                    <div style={{ color: '#777' }}>Qty: { this.state.qty}</div>
+                    
+                    <div className="cart-item-actions">
+                         {/* Buttons */}
+                        <img alt="increase"
+                            className="action-icons"
+                            src="https://image.flaticon.com/icons/png/128/992/992651.png"
+                            onClick = {this.increaseQty}
+                        />
+                        <img alt="decrease"
+                            className="action-icons"
+                            src="https://image.flaticon.com/icons/png/128/104/104616.png"
+                            onClick = {this.decreaseQty}
+                        />
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/128/1214/1214428.png" />
+                    </div>
                 </div>
-                <div className="cart-item-actions">
-                    {/* Buttons */}
-                </div>
-"
             </div>
         )
     }
